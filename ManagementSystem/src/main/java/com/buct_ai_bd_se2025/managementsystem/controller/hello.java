bdb4c88022ab8f062c467e7a6801b5833e4307b7
YozoraTempest <Nightskykn@gmail.com>
2025-04-22 04:29:47 +0800

    测试框架

---
+++ ManagementSystem/src/main/java/com/buct_ai_bd_se2025/managementsystem/controller/hello.java
@@ -0,0 +1,13 @@
+package com.buct_ai_bd_se2025.managementsystem.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class hello {
+    @GetMapping
+    public String hello(){
+        return "hello";
+    }
+
+}
