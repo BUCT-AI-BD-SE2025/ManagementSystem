468aaef3fe554a44657d710e657e8127385ecc27
YozoraTempest <Nightskykn@gmail.com>
2025-04-22 03:41:57 +0800

    创建

---
+++ ManagementSystem/src/main/java/com/buct_ai_bd_se2025/managementsystem/ManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.buct_ai_bd_se2025.managementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagementSystemApplication.class, args);
+    }
+
+}
---
+++ ManagementSystem/src/test/java/com/buct_ai_bd_se2025/managementsystem/ManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buct_ai_bd_se2025.managementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
