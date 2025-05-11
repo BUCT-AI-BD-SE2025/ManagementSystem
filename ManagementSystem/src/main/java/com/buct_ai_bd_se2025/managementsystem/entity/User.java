1755239da9e27482e6d280078196694ee7b4ceac
YOZORA <Nightskykn@gmail.com>
2025-05-11 09:11:02 +0800

    简单创建用户模块

--- ManagementSystem/src/main/java/com/buct_ai_bd_se2025/managementsystem/entity/User.java
+++ ManagementSystem/src/main/java/com/buct_ai_bd_se2025/managementsystem/entity/User.java
@@ -1,43 +1,152 @@
 package com.buct_ai_bd_se2025.managementsystem.entity;
 
-import jakarta.persistence.*;
-import lombok.Data;
-import org.hibernate.annotations.UpdateTimestamp;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import java.time.LocalDateTime;
+import lombok.Data;
 
-@Entity
-@Table(name = "user",
-        indexes = {
-                @Index(name = "idx_username", columnList = "username"),
-                @Index(name = "idx_email", columnList = "email"),
-                @Index(name = "idx_status", columnList = "status")
-        }
-)
+/**
+ * 
+ * @TableName user
+ */
+@TableName(value ="user")
 @Data
 public class User {
+    /**
+     * 
+     */
+    @TableId
+    private String uid;
 
-    @Id
-    private Long id;
-
+    /**
+     * 登陆用用户名
+     */
     private String username;
 
+    /**
+     * 昵称
+     */
+    private String nickname;
+
+    /**
+     * BCrypt加密
+     */
+    private String passwordHash;
+
+    /**
+     * 电子邮箱
+     */
     private String email;
 
-    private String password;
+    /**
+     * 手机号
+     */
+    private String phone;
 
-    private String nickname = "";
+    /**
+     * 0男1女2未知
+     */
+    private String sex;
 
-    private String avatar;
+    /**
+     * IP地址
+     */
+    private String ip;
 
-    private Boolean status = true;
+    /**
+     * 图片URL
+     */
+    private String avatarUrl;
 
-    private Boolean isLocked = false;
+    /**
+     * 
+     */
+    private Object status;
 
-    private Boolean isEmailVerified = false;
+    /**
+     * 
+     */
+    private LocalDateTime lastLogin;
 
-    private LocalDateTime registerTime;
+    /**
+     * 
+     */
+    private LocalDateTime createdAt;
 
-    private LocalDateTime lastLoginTime;
+    /**
+     * 
+     */
+    private LocalDateTime updatedAt;
 
-    private LocalDateTime updateTime;
+    @Override
+    public boolean equals(Object that) {
+        if (this == that) {
+            return true;
+        }
+        if (that == null) {
+            return false;
+        }
+        if (getClass() != that.getClass()) {
+            return false;
+        }
+        User other = (User) that;
+        return (this.getUid() == null ? other.getUid() == null : this.getUid().equals(other.getUid()))
+            && (this.getUsername() == null ? other.getUsername() == null : this.getUsername().equals(other.getUsername()))
+            && (this.getNickname() == null ? other.getNickname() == null : this.getNickname().equals(other.getNickname()))
+            && (this.getPasswordHash() == null ? other.getPasswordHash() == null : this.getPasswordHash().equals(other.getPasswordHash()))
+            && (this.getEmail() == null ? other.getEmail() == null : this.getEmail().equals(other.getEmail()))
+            && (this.getPhone() == null ? other.getPhone() == null : this.getPhone().equals(other.getPhone()))
+            && (this.getSex() == null ? other.getSex() == null : this.getSex().equals(other.getSex()))
+            && (this.getIp() == null ? other.getIp() == null : this.getIp().equals(other.getIp()))
+            && (this.getAvatarUrl() == null ? other.getAvatarUrl() == null : this.getAvatarUrl().equals(other.getAvatarUrl()))
+            && (this.getStatus() == null ? other.getStatus() == null : this.getStatus().equals(other.getStatus()))
+            && (this.getLastLogin() == null ? other.getLastLogin() == null : this.getLastLogin().equals(other.getLastLogin()))
+            && (this.getCreatedAt() == null ? other.getCreatedAt() == null : this.getCreatedAt().equals(other.getCreatedAt()))
+            && (this.getUpdatedAt() == null ? other.getUpdatedAt() == null : this.getUpdatedAt().equals(other.getUpdatedAt()));
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((getUid() == null) ? 0 : getUid().hashCode());
+        result = prime * result + ((getUsername() == null) ? 0 : getUsername().hashCode());
+        result = prime * result + ((getNickname() == null) ? 0 : getNickname().hashCode());
+        result = prime * result + ((getPasswordHash() == null) ? 0 : getPasswordHash().hashCode());
+        result = prime * result + ((getEmail() == null) ? 0 : getEmail().hashCode());
+        result = prime * result + ((getPhone() == null) ? 0 : getPhone().hashCode());
+        result = prime * result + ((getSex() == null) ? 0 : getSex().hashCode());
+        result = prime * result + ((getIp() == null) ? 0 : getIp().hashCode());
+        result = prime * result + ((getAvatarUrl() == null) ? 0 : getAvatarUrl().hashCode());
+        result = prime * result + ((getStatus() == null) ? 0 : getStatus().hashCode());
+        result = prime * result + ((getLastLogin() == null) ? 0 : getLastLogin().hashCode());
+        result = prime * result + ((getCreatedAt() == null) ? 0 : getCreatedAt().hashCode());
+        result = prime * result + ((getUpdatedAt() == null) ? 0 : getUpdatedAt().hashCode());
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", uid=").append(uid);
+        sb.append(", username=").append(username);
+        sb.append(", nickname=").append(nickname);
+        sb.append(", passwordHash=").append(passwordHash);
+        sb.append(", email=").append(email);
+        sb.append(", phone=").append(phone);
+        sb.append(", sex=").append(sex);
+        sb.append(", ip=").append(ip);
+        sb.append(", avatarUrl=").append(avatarUrl);
+        sb.append(", status=").append(status);
+        sb.append(", lastLogin=").append(lastLogin);
+        sb.append(", createdAt=").append(createdAt);
+        sb.append(", updatedAt=").append(updatedAt);
+        sb.append("]");
+        return sb.toString();
+    }
 }
