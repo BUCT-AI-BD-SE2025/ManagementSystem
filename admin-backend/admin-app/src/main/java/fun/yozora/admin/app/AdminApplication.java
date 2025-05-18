ce1537151d13e65def9a32105e7636600ae1036b
YozoraTempest <Nightskykn@gmail.com>
2025-05-18 17:55:17 +0800

    重命名

--- admin-backend/admin-app/src/main/java/fun/yozora/admin/app/AdminApplication.java
+++ admin-backend/admin-app/src/main/java/fun/yozora/admin/app/AdminApplication.java
@@ -3,14 +3,18 @@ package fun.yozora.admin.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @MapperScan("fun.yozora.admin.core.mapper")
 @ComponentScan("fun.yozora.admin")
+@EnableJpaRepositories("fun.yozora")
+@EntityScan("fun.yozora")
 @SpringBootApplication
-public class AdminAppApplication {
+public class AdminApplication {
     public static void main(String[] args) {
-        SpringApplication.run(AdminAppApplication.class, args);
+        SpringApplication.run(AdminApplication.class, args);
     }
 
 }
