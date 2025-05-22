3ae42eeb1cc06722f560a92433d92f2c178c1817
YozoraTempest <Nightskykn@gmail.com>
2025-05-22 21:03:07 +0800

    更内容审核结果

---
+++ admin-backend/admin-utils/src/main/java/fun/yozora/admin/utils/mapper/TextModerationResultMapper.java
@@ -0,0 +1,18 @@
+package fun.yozora.admin.utils.mapper;
+
+import fun.yozora.admin.utils.entity.TextModerationResult;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author Yozor
+* @description 针对表【text_moderation_result】的数据库操作Mapper
+* @createDate 2025-05-22 21:02:29
+* @Entity fun.yozora.admin.utils.entity.TextModerationResult
+*/
+public interface TextModerationResultMapper extends BaseMapper<TextModerationResult> {
+
+}
+
+
+
+
