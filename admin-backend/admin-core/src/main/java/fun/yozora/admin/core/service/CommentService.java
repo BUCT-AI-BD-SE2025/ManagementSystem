9e4309e15730e7a4b19247d0bf5dc92b69e65f73
YoruTempest <Nightskykn@gmail.com>
2025-05-22 12:57:49 +0800

    更新

---
+++ admin-backend/admin-core/src/main/java/fun/yozora/admin/core/service/CommentService.java
@@ -0,0 +1,13 @@
+package fun.yozora.admin.core.service;
+
+import fun.yozora.admin.domain.entity.Comment;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author YOZORA
+* @description 针对表【comment】的数据库操作Service
+* @createDate 2025-05-22 08:48:35
+*/
+public interface CommentService extends IService<Comment> {
+
+}
